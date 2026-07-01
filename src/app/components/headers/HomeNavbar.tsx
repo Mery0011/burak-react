5be66d46a47024b89567c12205db9c888d645c7d
mery <nasibajahon1995@gmail.com>
2026-07-01 14:03:57 +0900

    fix: homeNavbar menu part refactoring

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -5,30 +5,16 @@ export function HomeNavbar() {
     const authMember = null;
     return (
         <div className="home-navbar">
-            <Container sx={{ mt: "55px", height: "642px" }}>
-                <Stack
-                    direction="row"
-                    sx={{
-                        height: "50px",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                    }}
-                >
+            <Container className="navbar-container">
+                <Stack className="menu">
                     <Box>
                         <NavLink to="/">
-                            <img
-                                style={{ width: "125px", height: "30px" }}
+                            <img className="brand-logo"
                                 src="/icons/burak.svg"
                             />
                         </NavLink>
                     </Box>
-                    <Stack
-                        direction="row"
-                        sx={{
-                            justifyContent: "space-between",
-                            minWidth: "700px",
-                            alignItems: "center",
-                        }}
+                    <Stack className="links"
                     >
                         <Box className={"hover-line"}>
                             <NavLink to="/" activeClassName={"underline"}>
@@ -64,14 +50,15 @@ export function HomeNavbar() {
                         {!authMember ? (
                             <Box>
                                 <Button
-                                    variant="contained"
-                                    style={{ background: "#3776CC", color: "#f8f8ff" }}
+                                    variant="contained" className="login-button"
                                 >
                                     Login
                                 </Button>
                             </Box>
                         ) : (
-                            <img />
+                            <img className="user-avatar"
+                                src={"/icons/default-user.svg"}
+                                aria-haspopup={"true"} />
                         )}
                     </Stack>
                 </Stack>
