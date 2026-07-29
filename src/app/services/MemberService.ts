cca8181c22c5a636601f42bc3e0788915c9ce6f1
mery <nasibajahon1995@gmail.com>
2026-07-29 17:13:14 +0900

    fix: create login process

--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Member, MemberInput } from "../../lib/types/member";
+import { LoginInput, Member, MemberInput } from "../../lib/types/member";
 import { serverApi } from "../../lib/config";
 
 class MemberService {
@@ -48,6 +48,23 @@ class MemberService {
             return member;
         } catch (err) {
             console.log("Error, signup:", err);
+            throw err;
+        }
+    }
+
+    public async login(input: LoginInput): Promise<Member> {
+        try {
+            const url = this.path + "/member/login";
+            const result = await axios.post(url, input, { withCredentials: true });
+            console.log("login:", result);
+
+            const member: Member = result.data.member;
+            console.log("member:", member);
+            localStorage.setItem("memberData", JSON.stringify(member));
+
+            return member;
+        } catch (err) {
+            console.log("Error, login:", err);
             throw err;
         }
     }
